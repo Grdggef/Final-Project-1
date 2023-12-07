c36a250d2e351e4a6768674196fd69e39ff48f62
Grdggef <124305753+Grdggef@users.noreply.github.com>
2023-12-07 10:44:50 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ class Gui:
 
         self.voting_window = window
         self.candidate_frame = Frame(self.voting_window)
-        self.candidate_label = Label(self.candidate_frame, text='Voting')
+        self.candidate_label = Label(self.candidate_frame, text='Voting\n')
         self.candidate_label.pack(side='top')
         self.candidate_frame.pack(anchor='n')
 
@@ -19,33 +19,36 @@ class Gui:
         self.candidate1 = Radiobutton(self.option1, text='Brad', variable=self.var, value=1)
         self.candidate1_desc = Button(self.option1, text='Description', command=logic.description1)
         self.candidate1.pack()
-        self.candidate1_desc.pack()
-        self.option1.pack(anchor='w')
+        self.candidate1_desc.pack(pady=10)
+        self.option1.pack()
 
         self.option2 = Frame(self.voting_window)
         self.candidate2 = Radiobutton(self.option2, text='Charlie', variable=self.var, value=2)
         self.candidate2_desc = Button(self.option2, text='Description', command=logic.description2)
         self.candidate2.pack()
-        self.candidate2_desc.pack()
-        self.option2.pack(anchor='w')
+        self.candidate2_desc.pack(pady=10)
+        self.option2.pack()
 
         self.option3 = Frame(self.voting_window)
         self.candidate3 = Radiobutton(self.option3, text='Cindy', variable=self.var, value=3)
         self.candidate3_desc = Button(self.option3, text='Description', command=logic.description3)
         self.candidate3.pack()
-        self.candidate3_desc.pack()
-        self.option3.pack(anchor='w')
+        self.candidate3_desc.pack(pady=10)
+        self.option3.pack()
 
         self.buttons_frame = Frame(self.voting_window)
         self.submit = Button(self.buttons_frame, text='Submit', command=self.submit)
         self.clear = Button(self.buttons_frame, text='Clear', command=self.clear)
         self.tally_button = Button(self.buttons_frame, text='Tally', command=self.tally)
-        self.text = Label(self.buttons_frame, text="")
-        self.submit.pack(side='left')
-        self.clear.pack(side='left')
-        self.tally_button.pack(side='left')
-        self.text.pack(side='bottom')
-        self.buttons_frame.pack(anchor='w')
+        self.submit.pack(side='left',pady=5)
+        self.clear.pack(side='left',padx=15, pady=5)
+        self.tally_button.pack(side='left',pady=5)
+        self.buttons_frame.pack()
+
+        self.text_frame = Frame(self.voting_window)
+        self.text = Label(self.text_frame, text="")
+        self.text.pack(side='bottom', pady=15)
+        self.text_frame.pack(anchor='s')
 
     def submit(self):
         variable = self.var.get()
